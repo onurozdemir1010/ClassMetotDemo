662dd9f6d241bf27d663e560e3fcb5fa07341c31
Onur Özdemir <ozdemironur1010@gmail.com>
2021-01-19 23:05:32 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteriEkle)
+        {
+            Console.WriteLine(musteriEkle.Id +" No'lu Müşteri = "+musteriEkle.Ad +" " +musteriEkle.Soyad);
+        }
+        public void Sil(Musteri musteriSil)
+        {
+            Console.WriteLine(musteriSil.Id +" "+ musteriSil.Ad +" "+ musteriSil.Soyad +" Kişisi Bankamızın listesinden çıkarılmıştır");
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 10;
+            musteri1.Ad = "Onur";
+            musteri1.Soyad = "Özdemir";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 20;
+            musteri2.Ad = "Ahmet";
+            musteri2.Soyad = "Al";
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Id = 30;
+            musteri3.Ad = "Mehmet";
+            musteri3.Soyad = "Kaya";
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Ekle(musteri1);
+            musteriManager.Sil(musteri2);
+            
+        }
+
+
+
+    }
+}
